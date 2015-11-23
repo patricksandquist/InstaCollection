2d1af6a8a7ce0c6b019547fa3cb6571aa6ef6b87
patricksandquist <patricksandquist@gmail.com>
2015-11-23 11:35:46 -0800

    clean up Oath

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :collections, only: [:index, :show, :new, :create]
-  get '/oath/connect', to: 'oath#create'
-  get '/oath/callback', to: 'oath#index'
   root to: 'static_pages#root'
 end
